de6fadee2125ec61fbab4a4aaecb7f98f64325ec
aladeamidat1 <aladeamidatayomide@gmail.com>
2025-01-08 13:33:46 +0100

    first commit

---
+++ Squares.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Squares {
+	
+	public static void main(String[] args){
+	
+	Scanner input = new scanner(System.in);
+	
+	System.out.println("enter an input: ");
+
+		int number = input.nextInt();
+
+		
+
+
+
+}
+
+
+
+}
---
+++ compoundcalculator.py
@@ -0,0 +1,6 @@
+while (True):
+	investment = int(input("Enter initial investment: "))
+	if investment <= 0:
+		print("Invalid Input \nKindly enter again ")
+	else:
+		break

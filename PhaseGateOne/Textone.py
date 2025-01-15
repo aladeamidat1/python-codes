f24419d0e46cf35b2f7ddc627278e30a5b5f063f
aladeamidat1 <aladeamidatayomide@gmail.com>
2025-01-15 10:50:01 +0100

    save Textone

---
+++ PhaseGateOne/Textone.py
@@ -0,0 +1,12 @@
+number1 = int(input('Enter first integer: '))
+
+number2 = int(input('Enter second integer: '))
+
+Total = number1 + number2
+
+print ('the sum of ' , number1, 'and' , number2, 'is', Total)
+
+if Total <= 100:
+	print('True')
+else:
+	print('false')
